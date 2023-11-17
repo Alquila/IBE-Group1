1db9278523db249049ad3d460c99bb5f4f797c62
Alquila <athallenberg@gmail.com>
2023-11-17 09:45:25 +0100

    :)

---
+++ basic_ident.py
@@ -0,0 +1,79 @@
+from ecpy.elliptic_curve.pairing import gen_supersingular_ec, symmetric_weil_pairing
+from ecpy.fields import ExtendedFiniteField
+from ecpy.elliptic_curve import EllipticCurve
+import hashlib
+import random
+
+
+def bfh_parameter_generator(k):
+    # Should output (q, G1, G2, ê)
+    print(42)
+
+
+prim = 5
+order = -42
+F = ExtendedFiniteField(prim)
+E = EllipticCurve(F)
+P = E.random_point()
+
+
+def setup(k):
+    E, F, l = gen_supersingular_ec(k)
+
+
+# Eliptic curve E, points P,Q, m order of P,Q
+def useWeil(E, P, Q, m):
+    aa = symmetric_weil_pairing(E, P, Q, m)
+
+
+def h1(id):
+    """
+    Send element to G_1^*
+    Args:
+        id: element in {0,1}^*
+    Returns: element in G_1^*
+    """
+    hash = hashlib.sha256(id).hexdigest().encode("hex")
+    ee = int(hash, 16)
+    # TODO send to group
+    return hash
+
+
+def h2(g_id_r):
+    """
+    Sends element from G_2^* to {0,1}^n for some n
+    Args:
+        g_id_r: element in G_2^*
+    Returns: element in {0,1}^n
+    """
+    #  TODO send from group to {0,1}^n
+    return g_id_r
+
+
+def encrypt(m, id, P_pub):
+    # choose random r \in Z_q^*
+    r = random.randint(1, prim)
+    # Q_id = H1(id) \in G_1^*
+    Q_id = h1(id)
+    # Compute g_id = e(Q_id, P_pub) \in G_2^*
+    g_id = symmetric_weil_pairing(E, Q_id, P_pub, order)
+    # Compute g_id ^ r
+    g_id_r = g_id ** r
+    # Compute H2(g_id^r) \in {0,1}^n
+    h2_ = h2(g_id_r)
+    return r * P, m ^ h2_
+
+
+def decrypt(c, secret_key):
+    u, v = c
+
+    e_d_u = symmetric_weil_pairing(E, secret_key, u, order)  # element in G_2^*
+
+    h2_ = h2(e_d_u)  # element in {0,1}^n
+
+    return v ^ h2_  # m
+
+
+def extract(id, s):
+    q_id = h1(id)  # in G_2^*
+    return s*q_id
--- ecpy/elliptic_curve/pairing.py
+++ ecpy/elliptic_curve/pairing.py
@@ -1,6 +1,6 @@
 def miller(E, P, Q, m):
-  from six.moves import map
-  """
+    from six.moves import map
+    """
   Calculate Divisor by Miller's Algorithm
   Args:
     E: The Elliptic Curve
@@ -10,31 +10,33 @@ def miller(E, P, Q, m):
   Returns:
     f_P(Q)
   """
-  def h(P, Q, R):
-    # if \lambda is infinity
-    if (P == Q and P.y == 0) or (P != Q and P.x == Q.x):
-      return R.x - P.x
-    L = P.line_coeff(Q)
-    p = R.y - P.y - L * (R.x - P.x)
-    q = R.x + P.x + Q.x - L * L
-    return p / q
-  if P == Q:
-    return 1
-  b = map(int, bin(m)[2:])
-  next(b)
-  f = 1
-  T = P
-  for i in b:
-    f = f * f * h(T, T, Q)
-    T = T + T
-    if i:
-      f = f * h(T, P, Q)
-      T = T + P
-  return f
+
+    def h(P, Q, R):
+        # if \lambda is infinity
+        if (P == Q and P.y == 0) or (P != Q and P.x == Q.x):
+            return R.x - P.x
+        L = P.line_coeff(Q)
+        p = R.y - P.y - L * (R.x - P.x)
+        q = R.x + P.x + Q.x - L * L
+        return p / q
+
+    if P == Q:
+        return 1
+    b = map(int, bin(m)[2:])
+    next(b)
+    f = 1
+    T = P
+    for i in b:
+        f = f * f * h(T, T, Q)
+        T = T + T
+        if i:
+            f = f * h(T, P, Q)
+            T = T + P
+    return f
 
 
 def weil_pairing(E, P, Q, m, S=None):
-  """
+    """
   Calculate Weil Pairing
   Args:
     E: The Elliptic Curve
@@ -45,25 +47,25 @@ def weil_pairing(E, P, Q, m, S=None):
   Returns:
     e_m(P, Q)
   """
-  if S is None:
-    S = E.random_point()
-  from ecpy.utils.util import is_enable_native, _native
-  from ecpy.fields.ExtendedFiniteField import ExtendedFiniteFieldElement
-  if is_enable_native:
-    P = _native.EC_elem(E.ec, tuple(P.x), tuple(P.y), tuple(P.z))
-    Q = _native.EC_elem(E.ec, tuple(Q.x), tuple(Q.y), tuple(Q.z))
-    S = _native.EC_elem(E.ec, tuple(S.x), tuple(S.y), tuple(S.z))
-    if E.ec.type == 1:
-      t = _native.FF_elem(0)
-    elif E.ec.type == 2:
-      t = _native.EF_elem(0, 0)
-    _native.weil_pairing(t, E.ec, P, Q, S, m)
-    if E.ec.type == 1:
-      return t.to_python()
-    elif E.ec.type == 2:
-      t = t.to_python()
-      return ExtendedFiniteFieldElement(E.field, t[0], t[1])
-  else:
+    if S is None:
+        S = E.random_point()
+    # from ecpy.utils.util import is_enable_native, _native
+    # from ecpy.fields.ExtendedFiniteField import ExtendedFiniteFieldElement
+    # if is_enable_native:
+    #     P = _native.EC_elem(E.ec, tuple(P.x), tuple(P.y), tuple(P.z))
+    #     Q = _native.EC_elem(E.ec, tuple(Q.x), tuple(Q.y), tuple(Q.z))
+    #     S = _native.EC_elem(E.ec, tuple(S.x), tuple(S.y), tuple(S.z))
+    #     if E.ec.type == 1:
+    #         t = _native.FF_elem(0)
+    #     elif E.ec.type == 2:
+    #         t = _native.EF_elem(0, 0)
+    #     _native.weil_pairing(t, E.ec, P, Q, S, m)
+    #     if E.ec.type == 1:
+    #         return t.to_python()
+    #     elif E.ec.type == 2:
+    #         t = t.to_python()
+    #         return ExtendedFiniteFieldElement(E.field, t[0], t[1])
+    # else:
     fpqs = miller(E, P, Q + S, m)
     fps = miller(E, P, S, m)
     fqps = miller(E, Q, P - S, m)
@@ -71,39 +73,8 @@ def weil_pairing(E, P, Q, m, S=None):
     return E.field._inv(fps * fqps) * fpqs * fqs
 
 
-def tate_pairing(E, P, Q, m, k=2):
-  """
-  Calculate Tate Pairing
-  Args:
-    E: The Elliptic Curve
-    P: A point over E which has order m
-    Q: A point over E which has order m
-    m: The order of P, Q on E
-    k: [Optional] The Embedding Degree of m on E
-  """
-  from ecpy.utils.util import is_enable_native, _native
-  if is_enable_native:
-    P = _native.EC_elem(E.ec, tuple(P.x), tuple(P.y), tuple(P.z))
-    Q = _native.EC_elem(E.ec, tuple(Q.x), tuple(Q.y), tuple(Q.z))
-    if E.ec.type == 1:
-      t = _native.FF_elem(0)
-    elif E.ec.type == 2:
-      t = _native.EF_elem(0, 0)
-    _native.tate_pairing(t, E.ec, P, Q, m, k)
-    if E.ec.type == 1:
-      from ecpy.fields.Zmod import ZmodElement
-      return ZmodElement(E.field, t.to_python())
-    elif E.ec.type == 2:
-      from ecpy.fields.ExtendedFiniteField import ExtendedFiniteFieldElement
-      t = t.to_python()
-      return ExtendedFiniteFieldElement(E.field, t[0], t[1])
-  else:
-    f = miller(E, P, Q, m)
-    return f ** (((E.field.p ** k) - 1) // m)
-
-
-def MapToPoint(E, y):
-  """
+def map_to_point(E, y):
+    """
   MapToPoint Function: Given by Boneh-Durfee's ID-based Encryption Paper.
   Args:
     E: The Elliptic Curve
@@ -112,14 +83,14 @@ def MapToPoint(E, y):
   Returns:
     Correspond point of y on E
   """
-  from ecpy.utils import cubic_root
-  x = cubic_root(y**2 - 1)
-  Q = E(x, y)
-  return 6 * Q
+    from ecpy.utils import cubic_root
+    x = cubic_root(y ** 2 - 1)
+    Q = E(x, y)
+    return 6 * Q
 
 
 def gen_supersingular_ec(bits=70):
-  """
+    """
   Generate Super-Singluar Elliptic Curve
   Args:
     bits: The Security Parameter: log_2 p = bits
@@ -128,43 +99,43 @@ def gen_supersingular_ec(bits=70):
     A (Super Singular) Elliptic Curve, Extended Finite Field, l
     l is need to calculate Pairing
   """
-  from ecpy.fields import ExtendedFiniteField
-  from .EllipticCurve import EllipticCurve
+    from ecpy.fields import ExtendedFiniteField
+    from .EllipticCurve import EllipticCurve
 
-  def _next_prime(n):
-    from ecpy.util import is_prime
-    """
+    def _next_prime(n):
+        from ecpy.utils import is_prime
+        """
     return next prime of n
     """
-    while not is_prime(n):
-      n += 1
-    return n
+        while not is_prime(n):
+            n += 1
+        return n
 
-  """
+    """
   If you have gmpy, use gmpy.next_prime
   in other hand, use slow function
   """
-  try:
-    from gmpy import next_prime
-  except:
-    next_prime = _next_prime
+    try:
+        from gmpy import next_prime
+    except:
+        next_prime = _next_prime
 
-  def gen_prime():
-    from ecpy.util import is_prime
-    from random import randint
-    while True:
-      p = int(next_prime(randint(2**(bits - 1), 2**bits)))
-      if is_prime(p * 6 - 1):
-        break
-    return p * 6 - 1, p
+    def gen_prime():
+        from ecpy.utils import is_prime
+        from random import randint
+        while True:
+            p = int(next_prime(randint(2 ** (bits - 1), 2 ** bits)))
+            if is_prime(p * 6 - 1):
+                break
+        return p * 6 - 1, p
 
-  p, l = gen_prime()
-  F = ExtendedFiniteField(p, "x^2+x+1")
-  return EllipticCurve(F, 0, 1), F, l
+    p, l = gen_prime()
+    F = ExtendedFiniteField(p, "x^2+x+1")
+    return EllipticCurve(F, 0, 1), F, l
 
 
 def find_point_by_order(E, l):
-  """
+    """
   Find a Elliptic Curve Point P which has order l.
   Args:
     E: The Elliptic Curve
@@ -173,18 +144,18 @@ def find_point_by_order(E, l):
   Returns:
     Point on E which has order l.
   """
-  i = 3
-  while True:
-    r = E.get_corresponding_y(i)
-    if r != None:
-      P = E(i, r)
-      if (P * l).is_infinity():
-        return P
-    i += 1
+    i = 3
+    while True:
+        r = E.get_corresponding_y(i)
+        if r != None:
+            P = E(i, r)
+            if (P * l).is_infinity():
+                return P
+        i += 1
 
 
 def symmetric_weil_pairing(E, P, Q, m):
-  """
+    """
   Symmetric Weil Pairing
   \hat{e}(P, Q) = e(P, \phi(Q)) (\phi is Distortion Map)
   Args:
@@ -193,18 +164,4 @@ def symmetric_weil_pairing(E, P, Q, m):
     Q: A point on E which has order m
     m: The order of P, Q
   """
-  return weil_pairing(E, P, Q.distortion_map(), m)
-
-
-def symmetric_tate_pairing(E, P, Q, m, k=2):
-  """
-  Symmetric Tate Pairing
-  \hat{e}(P, Q) = e(P, \phi(Q)) (\phi is Distortion Map)
-  Args:
-    E: The Elliptic Curve
-    P: A point on E which has order m
-    Q: A point on E which has order m
-    m: The order of P, Q
-    k: [Optional] The Embedding Degree of m on E
-  """
-  return tate_pairing(E, P, Q.distortion_map(), m)
+    return weil_pairing(E, P, Q.distortion_map(), m)
