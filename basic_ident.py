7070ed4db45df44fddd599fda7607d7859dd4b75
Alquila <athallenberg@gmail.com>
2023-11-23 13:15:04 +0100

    stuff

--- basic_ident.py
+++ basic_ident.py
@@ -5,16 +5,23 @@ import hashlib
 import random
 
 
-def bfh_parameter_generator(k):
-    # Should output (q, G1, G2, ê)
-    print(42)
+def bdh_parameter_generator():
+    q = int("501794446334189957604282155189438160845433783392772743395579628617109"
+            "929160215221425142482928909270259580854362463493326988807453595748573"
+            "76419559953437557")
+    F = ExtendedFiniteField(q, "x^2+x+1")
+    E = EllipticCurve(F, 0, 1)
+    P = E(3, int("1418077311270457886139292292020587683642898636677353664354101171"
+                 "7684401801069777797699258667061922178009879315047772033936311133"
+                 "535564812495329881887557081"))
 
-
-prim = 5
-order = -42
-F = ExtendedFiniteField(prim)
-E = EllipticCurve(F)
-P = E.random_point()
+    s = int("129862491850266001914601437161941818413833907050695770313188660767"
+            "152646233571458109764766382285470424230719843324368007925375351295"
+            "39576510740045312772012")
+    sP = E(s,
+           int("452543250979361708074026409576755302296698208397782707067096515523"
+               "033579018123253402743775747767548650767928190884624134827869137911"
+               "24188897792458334596297"))
 
 
 def setup(k):
@@ -26,11 +33,12 @@ def useWeil(E, P, Q, m):
     aa = symmetric_weil_pairing(E, P, Q, m)
 
 
-def h1(id):
+def h1(id, P):
     """
     Send element to G_1^*
     Args:
         id: element in {0,1}^*
+        P: Generator for G_1
     Returns: element in G_1^*
     """
     hash = hashlib.sha256(str(id).encode("utf-8")).hexdigest()
@@ -51,13 +59,27 @@ def h2(g_id_r):
     return g_id_r
 
 
-def encrypt(m, id, P_pub):
-    # choose random r \in Z_q^*
-    r = random.randint(1, prim)
+def encrypt(E, P, m, id, P_pub, q, order):
+    """
+    Encrypt the message m
+    Args:
+        E: The Elliptic curve
+        P: The generator
+        m: the message
+        id: The ID of the receiver
+        P_pub: The public something
+        q: The prime order of G_1 G_2
+        order: The order of something ??
+
+    Returns: Encryption of the form (U,V) in G_1^* x {0,1}^n
+
+    """
     # Q_id = H1(id) \in G_1^*
     Q_id = h1(id)
+    # choose random r \in Z_q^*
+    r = random.randint(1, q)
     # Compute g_id = e(Q_id, P_pub) \in G_2^*
-    g_id = symmetric_weil_pairing(E, Q_id, P_pub, order)
+    g_id = symmetric_weil_pairing(E, Q_id, P_pub, q)  # TODO is order of G_1 G_2 q or order=(q+1)/6
     # Compute g_id ^ r
     g_id_r = g_id ** r
     # Compute H2(g_id^r) \in {0,1}^n
@@ -65,10 +87,21 @@ def encrypt(m, id, P_pub):
     return r * P, m ^ h2_
 
 
-def decrypt(c, secret_key):
+def decrypt(E, c, secret_key_d_id, order):
+    """
+    Decrypt the ciphertext
+    Args:
+        E: The Elliptic curve
+        c: Ciphertext (U,V)
+        secret_key_d_id: d_ID = s*H_1(ID)
+        order: order of the prime ?
+
+    Returns: the encrypted msg m
+
+    """
     u, v = c
 
-    e_d_u = symmetric_weil_pairing(E, secret_key, u, order)  # element in G_2^*
+    e_d_u = symmetric_weil_pairing(E, secret_key_d_id, u, order)  # e(d_id, U) element in G_2^*
 
     h2_ = h2(e_d_u)  # element in {0,1}^n
 
@@ -76,5 +109,8 @@ def decrypt(c, secret_key):
 
 
 def extract(id, s):
-    q_id = h1(id)  # in G_2^*
-    return s*q_id
+    """"
+    Given string ID in {0,1}^*, compute Q_ID = H_1(ID) in G_1^* and set private key d_id to be s*Q_ID
+    """
+    q_id = h1(id)  # in G_1^*
+    return s * q_id  # d_ID
--- basic_ident_test.py
+++ basic_ident_test.py
@@ -10,19 +10,15 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, False)  # add assertion here
 
     def test_field(self):
-        #23
-        #p = gen_prime_mod_four(10)
         p = int("501794446334189957604282155189438160845433783392772743395579628617109"
                 "929160215221425142482928909270259580854362463493326988807453595748573"
                 "76419559953437557")
-        # print("p: ", p)
-        # print("l", l)
         F = ExtendedFiniteField(p, "x^2+x+1")
         print("F.t: ", F.t)
         print("F.p: ", F.p)
         print("F.n: ", F.n)
         print("order: ", F.order())
-        E = EllipticCurve(F, 0,1)
+        E = EllipticCurve(F, 0, 1)
         print(E)
         print(E.field)
         print("random")
@@ -32,19 +28,32 @@ class MyTestCase(unittest.TestCase):
         print(E.get_corresponding_y(7))
         self.assertEqual(True, True)  # add assertion here
 
-
     def test_what_happens_when_hash(self):
         m = 'iamIDstring'
         hash_ = hashlib.sha256(str(m).encode("utf-8")).hexdigest()
         ee = int(hash_, 16)  # 16 because in Hex base
         print(hash_)  # is hex encoded string
         print(ee)  # is an integer
+        # TODO what happens when we do ee*P ? Is that then a group element?
+        p = int("501794446334189957604282155189438160845433783392772743395579628617109"
+                "929160215221425142482928909270259580854362463493326988807453595748573"
+                "76419559953437557")
+        F = ExtendedFiniteField(p, "x^2+x+1")
+        E = EllipticCurve(F, 0, 1)
+        P = E(3, int("1418077311270457886139292292020587683642898636677353664354101171"
+                     "7684401801069777797699258667061922178009879315047772033936311133"
+                     "535564812495329881887557081"))
+        print(ee * P)
         self.assertEqual(True, True)  # add assertion here
 
     def test_what_happens_with_eliptic(self):
         p = gen_prime_mod_four(14)
         print(p)
 
+#  TODO find out what this does on input H(E.field(symmetric_tate_pairing(E, sP, pubkey, l) ** r))) = H2(g_id^r)
+#  TODO find out what E.field(symmetric_tate_pairing(E, sP, pubkey, l) ** r)) is of type
+def H(x):
+    return x.x * x.field.p + x.y
 
 
 if __name__ == '__main__':
